e44a01d68662528ebb2b7a4cf1f3354ebdd7f536
Wannamiu <58030218@kmitl.ac.th>
2017-11-04 20:49:30 +0700

    commited by wannisa

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,22 +6,59 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Student su = new Student();
-            su.Name = "Student Name";
-            su.ID = "12345678";
-            su.GPA = 7.5f;
-            Console.WriteLine("Student name : " + su.Name);
-            Console.WriteLine("Student ID   : " + su.ID);
-            Console.WriteLine("Student GPA  : " + su.GPA);
+            Student su = new Student("Student Name", "12345678");
+            try
+            {
+               // su.Name = "My name";
+                su.GPA = 4.0f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
     class Student
     {
-        private string Name;
-        private string ID;
-        private float GPA;
+        private string name;
+        private string id;
+        private float gpa;
 
+        public Student(string Name, string ID)
+        {
+            this.name = Name;   // assign auto variable to the field
+            this.id = ID;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string ID
+        {
+            get { return id; }
+        }
+        public float GPA
+        {
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
+        }
     }
+
+
+
 
 }
